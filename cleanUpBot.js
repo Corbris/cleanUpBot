2fdd0c80cd4975b0926af5133a284191ff772dda
Kevin Lucas <ktlucas03@gmail.com>
2018-12-13 20:45:45 -0600

    changed to the discord.js library

---
+++ cleanUpBot.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+var auth = require('./auth.json');
+const bot = new Discord.Client();
+
+bot.login(auth.token);
+ 
+bot.on("ready", () => {
+    console.log('Connected');
+    console.log('Logged in as: ');
+    console.log(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on("message", (message) => {
+	if (message.content.indexOf('!') !== 0) return;  //does not start with '!'
+	
+    const args = message.content.slice(1).trim().split(/ +/g);
+    const cmd = args.shift();
+	console.log(cmd + ' ' + args);
+	
+	switch(cmd) 
+	{
+		case 'cleanUp':
+			message.channel.send("deleting each new messages after "+ args + " seconds.");
+		break;
+	}
+
+});
