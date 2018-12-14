94bc49a063b3292eddfe75e57f1e550f1460e7fe
Kevin Lucas <ktlucas03@gmail.com>
2018-12-13 21:59:21 -0600

    added error thowing for user input and maped out how to bot will work.

--- cleanUpBot.js
+++ cleanUpBot.js
@@ -16,12 +16,20 @@ bot.on("message", (message) => {
 	
     const args = message.content.slice(1).trim().split(/ +/g);
     const cmd = args.shift();
-
+	
+	
 	switch(cmd) 
 	{
 		case 'cleanUp':
-			message.channel.send("deleting each new messages after "+ args + " seconds.");
+			if(!message.member.permissions.has("ADMINISTRATOR")){message.channel.send("You don not have the permissions to do this."); return;}  //dont have admin.
+			if(args.length > 1 || isNaN(parseInt(args[0], 10))){message.channel.send("Request needs to be in the format of **!cleanUp {seconds}**"); return;}  //not in the right form
+			if(parseInt(args[0], 10) < 15){message.channel.send("Time needs to be at least 15 seconds"); return;} //need to be at least 15 seconds
+
+			message.channel.send("deleting all messages and each new messages after "+ parseInt(args[0], 10) + " seconds of the post time.");
+			//this is valid clear the whole channel for all post but pin post. 
 		break;
 	}
 
+	//is this message from a channel we want to delete from? (prob just use json, save channel ID and the time.)
+		//message.delete(time)
 });
